13e4308060b80346d77e3dff372e210dfcb56932
ANSHUMAN UPADHYAY <anshumanupy@gmail.com>
2017-07-10 18:45:26 +0530

    Commit 2

--- resumep3/js/helper.js
+++ resumep3/js/helper.js
@@ -1,17 +1,4 @@
-/*
 
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span id="name-role">%data%</span><hr>';
 
@@ -90,12 +77,6 @@ $(document).click(function(loc) {
 });
 
 
-
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
 var map;    // declares a global map variable
 
 
@@ -107,7 +88,8 @@ function initializeMap() {
   var locations;
 
   var mapOptions = {
-    disableDefaultUI: true
+    center: {lat: 28.6415, lng: 77.3714},
+    zoom: 8
   };
 
   /*
@@ -238,12 +220,11 @@ function initializeMap() {
 Uncomment the code below when you're ready to implement a Google Map!
 */
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+//Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+ map.fitBounds(mapBounds);
+});
--- resumep3/js/resumeBuilder.js
+++ resumep3/js/resumeBuilder.js
@@ -14,7 +14,33 @@ var bio = {
 
         skills: ["PHP", "CakePHP", "Python", "NodeJs", "Linux", "Perseverance", "Positive Thought Process", "Quick Learner and Ready to Unlearn!"],
 
-        biopic: "./images/resimg.jpg"
+        biopic: "./images/resimg.jpg",
+
+        display: function (){
+
+                //Forming the header of the resume here.
+
+                $("#header").append(HTMLheaderName.replace("%data%", bio.name));
+                $("#header").append(HTMLheaderRole.replace("%data%", bio.role));
+                $("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
+                $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.message));
+                if(bio.skills.length > 0){
+                        $("#header").append(HTMLskillsStart);
+
+                        bio.skills.forEach(function (skill){
+                                $("#skills").append(HTMLskills.replace("%data%", skill));
+
+                        });
+                }
+                //Replacement for #Header Ends here.
+                $("#topContacts").append(HTMLmobile.replace("%data%",bio.contacts.mob));
+                $("#topContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
+                $("#topContacts").append(HTMLtwitter.replace("%data%",bio.contacts.twitter));
+                $("#topContacts").append(HTMLgithub.replace("%data%",bio.contacts.github));
+                $("#topContacts").append(HTMLblog.replace("%data%",bio.contacts.blog));
+                $("#topContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
+
+        }
 };
 
 var education = {
@@ -51,97 +77,107 @@ var education = {
                         date: "In process",
                         url: "https://udacity.com"
                 }
-        ]
+        ],
+
+        display: function () {
+                $("#education").append(HTMLschoolStart);
+                education.schools.forEach(function (school){
+                        $("#education").append(HTMLschoolName.replace("%data%", school.name));
+                        $("#education").append(HTMLschoolDegree.replace("%data%", school.degree));
+                        $("#education").append(HTMLschoolDates.replace("%data%", school.date));
+                        $("#education").append(HTMLschoolLocation.replace("%data%", school.location));
+                        $("#education").append(HTMLschoolMajor.replace("%data%", school.major));
+
+                });
+        }
 };
 
 
 
 var work = {
-        employer: "Infosys Ltd.",
-        title: "Systems Engineer",
-        dates: "June 2017 to Present",
-        location: "Mysore(Karnataka) India.",
-        description: "I am Training at Infosys currently."
-};
+        jobs:
+        [       {
+                        employer: "Infosys Ltd.",
+                        title: "Systems Engineer",
+                        dates: "June 2017 to Present",
+                        location: "Mysore(Karnataka) India.",
+                        description: "I am Training at Infosys currently."
+                }
+        ],
 
-var projects = [
-         {
-                title: "Event Managment System In CakePHP(GLAeMS)",
-                dates: "September-2016 to May-2017",
-                description: "This was the major project of my Btech Academics. The concept of this project was simply to enforce a single platform to facilitate the registration and conduction of various events that are being held in my college. Admin is required to add the Event poster and make event online from his account. The rest will be taken care by the system. Students can now register in these events and get an OTP through email which they have to show for their attendance at the event. The System also generates PDF-report of the Event. Other utility functions and modules are also implemented to foster the smooth functioning and credibility of this online system.",
-                images: ["./images/ems/ems1.png", "./images/ems/ems2.png", "./images/ems/ems3.png", "./images/ems/ems4.png", "./images/ems/ems5.png", "./images/ems/ems6.png", "./images/ems/ems7.png", "./images/ems/ems8.png", "./images/ems/ems9.png", "./images/ems/ems10.png"]
-        },
-         {
-                title: "Hostel Management System in PHP",
-                dates: "Jan-2016 to May-2016",
-                description: "This project was the first experience of mine in the field of full stack web developement. The concept  of the project was simple database manipulations but I learnt web development from this endeavour. I also had my hands dirty with Bootstrap framework and this framework's response with respect to the PHP script and AJAX(using core JS) pulled me in the Web development world",
-                images: ["./images/hms/hms1.svg", "./images/hms/hms2.svg", "./images/hms/hms3.svg"]
-        },
-         {
-                title: "Udacity Responsive Frontend Portfolio",
-                dates: "July 2017",
-                description: "There were certain glitches when it came to frontend design. That is why I decided to take the Front-end course at Udacity.This is the most favorite project of mine as far as designing of the web page is concerned. The main motive of coding here was to make this website visible on any device it is opened while taking the \"Good-Looks\" of this website into concern. &#128513;",
-                images: []
+        display: function () {
+                $("#workExperience").append(HTMLworkStart);
+                work.jobs.forEach(function (job) {
+
+                        $("#workExperience").append(HTMLworkEmployer.replace("%data%", job.employer));
+                        $("#workExperience").append(HTMLworkTitle.replace("%data%", job.title));
+                        $("#workExperience").append(HTMLworkDates.replace("%data%", job.dates));
+                        $("#workExperience").append(HTMLworkLocation.replace("%data%", job.location));
+                        $("#workExperience").append(HTMLworkDescription.replace("%data%", job.description));
+
+                });
         }
-];
+};
+var project =
+{         projects: [
+                 {
+                        title: "Event Managment System In CakePHP(GLAeMS)",
+                        dates: "September-2016 to May-2017",
+                        description: "This was the major project of my Btech Academics. The concept of this project was simply to enforce a single platform to facilitate the registration and conduction of various events that are being held in my college. Admin is required to add the Event poster and make event online from his account. The rest will be taken care by the system. Students can now register in these events and get an OTP through email which they have to show for their attendance at the event. The System also generates PDF-report of the Event. Other utility functions and modules are also implemented to foster the smooth functioning and credibility of this online system.",
+                        images: ["./images/ems/ems1.png", "./images/ems/ems2.png", "./images/ems/ems3.png", "./images/ems/ems4.png", "./images/ems/ems5.png", "./images/ems/ems6.png", "./images/ems/ems7.png", "./images/ems/ems8.png", "./images/ems/ems9.png", "./images/ems/ems10.png"]
+                },
+                 {
+                        title: "Hostel Management System in PHP",
+                        dates: "Jan-2016 to May-2016",
+                        description: "This project was the first experience of mine in the field of full stack web developement. The concept  of the project was simple database manipulations but I learnt web development from this endeavour. I also had my hands dirty with Bootstrap framework and this framework's response with respect to the PHP script and AJAX(using core JS) pulled me in the Web development world",
+                        images: ["./images/hms/hms1.svg", "./images/hms/hms2.svg", "./images/hms/hms3.svg"]
+                },
+                 {
+                        title: "Udacity Responsive Frontend Portfolio",
+                        dates: "July 2017",
+                        description: "There were certain glitches when it came to frontend design. That is why I decided to take the Front-end course at Udacity.This is the most favorite project of mine as far as designing of the web page is concerned. The main motive of coding here was to make this website visible on any device it is opened while taking the \"Good-Looks\" of this website into concern. &#128513;",
+                        images: []
+                }
+        ],
+
+        display: function(){
+                $("#projects").append(HTMLprojectStart);
+                var index = 0;
+                project.projects.forEach(function (project) {
+                        index += 1;
+                        $("#projects").append("<div class='row' id='"+index+"'>");
+                        $("#"+index).append(HTMLprojectTitle.replace("%data%", project.title));
+
+                        $("#"+index).append(HTMLprojectDates.replace("%data%", project.dates));
+                        $("#"+index).append(HTMLprojectDescription.replace("%data%", project.description));
+                        project.images.forEach(function (url){
+                                $("#"+index).append(HTMLprojectImage.replace("%data%", url));
+
+                        });
+
+                        $("#projects").append("</div>");
+
+                });
+        }
+};
 //Variables other than objects may be defined below.
 
 
 
-//Forming the header of the resume here.
+//Calling "Bio's" display() below;
 
-$("#header").append(HTMLheaderName.replace("%data%", bio.name));
-$("#header").append(HTMLheaderRole.replace("%data%", bio.role));
-$("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
-$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.message));
-if(bio.skills.length > 0){
-        $("#header").append(HTMLskillsStart);
+bio.display();
 
-        bio.skills.forEach(function (skill){
-                $("#skills").append(HTMLskills.replace("%data%", skill));
+//Calling "Work's" display() below;
 
-        });
-}
-//Replacement for #Header Ends here.
-$("#topContacts").append(HTMLmobile.replace("%data%",bio.contacts.mob));
-$("#topContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
-$("#topContacts").append(HTMLtwitter.replace("%data%",bio.contacts.twitter));
-$("#topContacts").append(HTMLgithub.replace("%data%",bio.contacts.github));
-$("#topContacts").append(HTMLblog.replace("%data%",bio.contacts.blog));
-$("#topContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
-//Replacement for #topContacts ends here.
+work.display();
 
-$("#workExperience").append(HTMLworkStart);
-$("#workExperience").append(HTMLworkEmployer.replace("%data%", work.employer));
-$("#workExperience").append(HTMLworkTitle.replace("%data%", work.title));
-$("#workExperience").append(HTMLworkDates.replace("%data%", work.dates));
-$("#workExperience").append(HTMLworkLocation.replace("%data%", work.location));
-$("#workExperience").append(HTMLworkDescription.replace("%data%", work.description));
-//Replacement for #workExperience ends here.
+//Calling "Project's" display() below;
+project.display();
 
-$("#projects").append(HTMLprojectStart);
-var index = 0;
-projects.forEach(function (project) {
-        index += 1;
-        $("#projects").append("<div class='row' id='"+index+"'>");
-        $("#"+index).append(HTMLprojectTitle.replace("%data%", project.title));
+//Calling "Education's" display() below;
 
-        $("#"+index).append(HTMLprojectDates.replace("%data%", project.dates));
-        $("#"+index).append(HTMLprojectDescription.replace("%data%", project.description));
-        project.images.forEach(function (url){
-                $("#"+index).append(HTMLprojectImage.replace("%data%", url));
+education.display();
 
-        });
-
-        $("#projects").append("</div>");
-
-});
-$("#education").append(HTMLschoolStart);
-education.schools.forEach(function (school){
-        $("#education").append(HTMLschoolName.replace("%data%", school.name));
-        $("#education").append(HTMLschoolDegree.replace("%data%", school.degree));
-        $("#education").append(HTMLschoolDates.replace("%data%", school.date));
-        $("#education").append(HTMLschoolLocation.replace("%data%", school.location));
-        $("#education").append(HTMLschoolMajor.replace("%data%", school.major));
-
-});
+$("#mapDiv").append(googleMap);
+$("#map").append(map);
