7780ed9590bbfa184af2d7104b8337d7c5d30ab4
ANSHUMAN UPADHYAY <anshumanupy@gmail.com>
2017-07-10 23:44:48 +0530

    Final submit of resume

---
+++ profile/js/helper.js
@@ -0,0 +1,177 @@
+
+var HTMLheaderName = '<h1 id="name">%data%</h1>';
+var HTMLheaderRole = '<span id="name-role">%data%</span><hr>';
+
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>'; //Not yet clear; What shoudl I write in this.
+var HTMLmobile = '<li class="flex-item"><span class="orange-text"><i class="fa fa-mobile" aria-hidden="true"></i> mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text"><i class="fa fa-envelope-o" aria-hidden="true"></i> email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text"><i class="fa fa-twitter" aria-hidden="true"></i> twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text"><i class="fa fa-github" aria-hidden="true"></i> github</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text"><i class="fa fa-quora" aria-hidden="true"></i> blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text"><i class="fa fa-map-marker" aria-hidden="true"></i> location</span><span class="white-text">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic img-responsive">';
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+
+var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkEmployer = '<a href="#" target="_other">%data%';
+var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectTitle = '<a href="#" target="_other">%data%</a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<div class="col-md-6 col-xs-6"><img src="%data%" class="img-responsive"></div>';
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a href="#" target="_other">%data%';
+var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+
+var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineTitle = '<a href="#" target="_other">%data%';
+var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineURL = '<br><a href="#" target="_other">%data%</a>';
+
+var googleMap = '<div id="map"></div>';
+
+
+
+
+var map;    // declares a global map variable
+
+
+function initializeMap() {
+
+  var locations;
+
+  var mapOptions = {
+    center: {lat: 28.6415, lng: 77.3714},
+    zoom: 8
+  };
+
+
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+
+
+  function locationFinder() {
+
+    // initializes an empty array
+    var locations = [];
+
+    // adds the single location property from bio to the locations array
+    locations.push(bio.contacts.location);
+
+    education.schools.forEach(function(school){
+      locations.push(school.location);
+    });
+
+    work.jobs.forEach(function(job){
+      locations.push(job.location);
+    });
+
+    return locations;
+  }
+
+
+  function createMapMarker(placeData) {
+
+    // The next lines save location data from the search result object to local variables
+    var lat = placeData.geometry.location.lat();  // latitude from the place service
+    var lon = placeData.geometry.location.lng();  // longitude from the place service
+    var name = placeData.formatted_address;   // name of the place from the place service
+    var bounds = window.mapBounds;            // current boundaries of the map window
+
+    // marker is an object with additional data about the pin for a single location
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeData.geometry.location,
+      title: name
+    });
+
+    // infoWindows are the little helper windows that open when you click
+    // or hover over a pin on a map. They usually contain more information
+    // about a location.
+    var infoWindow = new google.maps.InfoWindow({
+      content: "<h2>"+name+"</h2>"
+    });
+
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.open(map, marker);
+    });
+
+    // this is where the pin actually gets added to the map.
+    // bounds.extend() takes in a map location object
+    bounds.extend(new google.maps.LatLng(lat, lon));
+    // fit the map to the new marker
+    map.fitBounds(bounds);
+    // center the map
+    map.setCenter(bounds.getCenter());
+  }
+
+  /*
+  callback(results, status) makes sure the search returned results for a location.
+  If so, it creates a new map marker for that location.
+  */
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMapMarker(results[0]);
+    }
+  }
+
+  /*
+  pinPoster(locations) takes in the array of locations created by locationFinder()
+  and fires off Google place searches for each location
+  */
+  function pinPoster(locations) {
+
+    // creates a Google place search service object. PlacesService does the work of
+    // actually searching for location data.
+    var service = new google.maps.places.PlacesService(map);
+
+    // Iterates through the array of locations, creates a search object for each location
+      locations.forEach(function(place){
+      // the search request object
+      var request = {
+        query: place
+      };
+
+      // Actually searches the Google Maps API for location data and runs the callback
+      // function with the search results after each search.
+      service.textSearch(request, callback);
+    });
+  }
+
+  // Sets the boundaries of the map based on pin locations
+  window.mapBounds = new google.maps.LatLngBounds();
+
+  // locations is an array of location strings returned from locationFinder()
+  locations = locationFinder();
+
+  // pinPoster(locations) creates pins on the map for each location in
+  // the locations array
+  pinPoster(locations);
+
+}
+
+
+
+//Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
+
+// Vanilla JS way to listen for resizing of the window
+// and adjust map bounds
+window.addEventListener('resize', function(e) {
+ map.fitBounds(mapBounds);
+});
---
+++ profile/js/resumeBuilder.js
@@ -0,0 +1,208 @@
+var bio = {
+    name: "Anshuman Upadhyay",
+    role: "Full Stack Web Developer",
+    contacts: {
+        mob: "+91-9837125424",
+        email: "anshumanupy@gmail.com",
+        twitter: "https://goo.gl/NDGXGW",
+        github: "https://goo.gl/fjq1Z1",
+        blog: "https://goo.gl/2hB1Lj",
+        location: "Ghaziabad(Uttar Pradesh) India"
+    },
+    message: "Hi! Welcome to my profile. All details here are authentic and there is more to explore for you if you contact me!",
+
+    skills: ["PHP", "CakePHP", "Python", "NodeJs", "Linux", "Perseverance", "Positive Thought Process", "Quick Learner and Ready to Unlearn!"],
+
+    biopic: "./images/resimg.jpg",
+
+    display: function () {
+
+        //Forming the header of the resume here.
+
+        $("#header").append(HTMLheaderName.replace("%data%", bio.name));
+        $("#header").append(HTMLheaderRole.replace("%data%", bio.role));
+        $("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
+        $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.message));
+        if (bio.skills.length > 0) {
+            $("#header").append(HTMLskillsStart);
+
+            bio.skills.forEach(function (skill) {
+                $("#skills").append(HTMLskills.replace("%data%", skill));
+
+            });
+        }
+        //Replacement for #Header Ends here.
+
+        $("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mob));
+        $("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+        $("#topContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+        $("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+        $("#topContacts").append(HTMLblog.replace("%data%", bio.contacts.blog));
+        $("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+
+        $("#footerContacts").append(HTMLmobile.replace("%data%", bio.contacts.mob));
+        $("#footerContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+        $("#footerContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+        $("#footerContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+        $("#footerContacts").append(HTMLblog.replace("%data%", bio.contacts.blog));
+        $("#footerContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+
+    }
+};
+
+var education = {
+    schools: [{
+            name: "High School",
+            degree: "N/A",
+            date: "May 2011",
+            location: "Guwahati(Assam) India",
+            major: "N/A",
+            url: "#"
+        },
+        {
+            name: "Plus 2",
+            degree: "N/A",
+            date: "May 2013",
+            location: "Ghaziabad(Uttar Pradesh) India",
+            major: "Science",
+            url: "https://www.stteresaschool.in/"
+        },
+        {
+            name: "College",
+            degree: "BTech.",
+            date: "May 2017",
+            location: "Mathura(Uttar Pradesh) India",
+            major: "Computer Science",
+            url: "http://gla.ac.in"
+        }
+
+
+    ],
+
+    online_courses: [{
+        title: "Front-end Web Developer",
+        school: "Udacity",
+        date: "In process",
+        url: "https://goo.gl/1LxRCg"
+    },
+
+    {
+        title: "Development with NodeJs",
+        school: "Udemy",
+        date: "In process",
+        url: "https://goo.gl/f1m6G4"
+    }
+    ],
+
+    display: function () {
+        $("#education").append(HTMLschoolStart);
+        education.schools.forEach(function (school) {
+            $("#education").append((HTMLschoolName.replace("%data%", school.name)).replace("#", school.url));
+            $("#education").append(HTMLschoolDegree.replace("%data%", school.degree));
+            $("#education").append(HTMLschoolDates.replace("%data%", school.date));
+            $("#education").append(HTMLschoolLocation.replace("%data%", school.location));
+            $("#education").append(HTMLschoolMajor.replace("%data%", school.major));
+
+        });
+        $("#education").append(HTMLonlineClasses);
+        education.online_courses.forEach(function (course) {
+            $("#education").append(HTMLonlineTitle.replace("%data%", course.title));
+            $("#education").append(HTMLonlineSchool.replace("%data%", course.school));
+            $("#education").append(HTMLonlineDates.replace("%data%", course.date));
+            $("#education").append((HTMLonlineURL.replace("%data%", '<i class="fa fa-link" aria-hidden="true"></i> To-Site')).replace("#", course.url));
+        });
+    }
+};
+
+
+
+
+var work = {
+    jobs: [{
+        employer: "Infosys Ltd.",
+        title: "Systems Engineer",
+        dates: "Anytime soon",
+        location: "Mysore(Karnataka) India.",
+        description: "I will be Training at Infosys shortly.",
+        url: "https://www.infosys.com/"
+    }],
+
+    display: function () {
+        $("#workExperience").append(HTMLworkStart);
+        work.jobs.forEach(function (job) {
+
+            $("#workExperience").append((HTMLworkEmployer.replace("%data%", job.employer)).replace("#", job.url));
+            $("#workExperience").append(HTMLworkTitle.replace("%data%", job.title));
+            $("#workExperience").append(HTMLworkDates.replace("%data%", job.dates));
+            $("#workExperience").append(HTMLworkLocation.replace("%data%", job.location));
+            $("#workExperience").append(HTMLworkDescription.replace("%data%", job.description));
+
+        });
+    }
+};
+
+var project = {
+    projects: [{
+            title: "Event Managment System In CakePHP(GLAeMS)",
+            dates: "September-2016 to May-2017",
+            description: "This was the major project of my Btech Academics. The concept of this project was simply to enforce a single platform to facilitate the registration and conduction of various events that are being held in my college. Admin is required to add the Event poster and make event online from his account. The rest will be taken care by the system. Students can now register in these events and get an OTP through email which they have to show for their attendance at the event. The System also generates PDF-report of the Event. Other utility functions and modules are also implemented to foster the smooth functioning and credibility of this online system.",
+            images: ["./images/ems/ems1.png", "./images/ems/ems2.png", "./images/ems/ems3.png", "./images/ems/ems4.png", "./images/ems/ems5.png", "./images/ems/ems6.png", "./images/ems/ems7.png", "./images/ems/ems8.png", "./images/ems/ems9.png", "./images/ems/ems10.png"],
+            url: "https://goo.gl/hm1AbY"
+        },
+        {
+            title: "Hostel Management System in PHP",
+            dates: "Jan-2016 to May-2016",
+            description: "This project was the first experience of mine in the field of full stack web developement. The concept  of the project was simple database manipulations but I learnt web development from this endeavour. I also had my hands dirty with Bootstrap framework and this framework's response with respect to the PHP script and AJAX(using core JS) pulled me in the Web development world",
+            images: ["./images/hms/hms1.svg", "./images/hms/hms2.svg", "./images/hms/hms3.svg"],
+            url: "https://goo.gl/X7aSVU"
+        },
+        {
+            title: "Udacity Responsive Frontend Portfolio",
+            dates: "July 2017",
+            description: "There were certain glitches when it came to frontend design. That is why I decided to take the Front-end course at Udacity.This is the most favorite project of mine as far as designing of the web page is concerned. The main motive of coding here was to make this website visible on any device it is opened while taking the \"Good-Looks\" of this website into concern. &#128513;",
+            images: [],
+            url: "https://anshup7.github.io/portfolio/project/"
+        }
+    ],
+
+    display: function () {
+        $("#projects").append(HTMLprojectStart);
+        var index = 0;
+        project.projects.forEach(function (project) {
+            index += 1;
+            $("#projects").append("<div class='row' id='" + index + "'>");
+            $("#" + index).append((HTMLprojectTitle.replace("%data%", project.title)).replace("#",project.url));
+
+            $("#" + index).append(HTMLprojectDates.replace("%data%", project.dates));
+            $("#" + index).append(HTMLprojectDescription.replace("%data%", project.description));
+            project.images.forEach(function (url) {
+                $("#" + index).append(HTMLprojectImage.replace("%data%", url));
+
+            });
+
+            $("#projects").append("</div>");
+
+        });
+    }
+};
+
+
+
+
+//Calling "Bio's" display() below;
+
+bio.display();
+
+//Calling "Work's" display() below;
+
+work.display();
+
+//Calling "Project's" display() below;
+project.display();
+
+//Calling "Education's" display() below;
+
+education.display();
+
+$("#mapDiv").append(googleMap);
+$("#map").append(map);
